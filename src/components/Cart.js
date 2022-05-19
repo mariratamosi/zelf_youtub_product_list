2561751fb505160b08ba0223c0da7e2bd0e2a517
marira <rongrazzo@gmail.com>
2022-05-20 04:14:20 +0600

    wip

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import "../styles/Cart.scss"
 import Product from "./Product"
+import downvote from "../assets/downvote.svg"
 
 import { useEffect } from "react"
 
@@ -24,7 +25,12 @@ const Cart = ({ items, subtotal, title }) => {
           <div className="select-loader"></div>
         </div>
         <div className="cart-and-title">
-          <div className="cart-title">{title}</div>
+          <div className="cart-title-collapsible">
+            <div className="yz-icon">
+              <img src={downvote} alt="Collapse"></img>
+            </div>
+            <div className="title">{title}</div>
+          </div>
           <div className="cart-items">
             {Object.keys(items).map((key, idx) => (
               <Product key={idx} item={items[key]}></Product>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,45 @@
 import "../styles/Product.scss"
+import downvote from "../assets/downvote.svg"
 
 const Product = ({ item }) => {
   if (!item) return <div>Loading...</div>
 
   // creator_banner_url, creator_icon_url, creator_name, creator_subscriber_count
 
-  return <>{item.name}</>
+  return (
+    <div className="sc-item">
+      <div className="sc-item-image">
+        <img src={item.image.replace("jpeg", "png")} alt="Collapse"></img>
+      </div>
+      <div className="desc-and-btns">
+        <div className="sc-item-short-desc">
+          <div className="desc-left">
+            <div>
+              <span className="sold-by-text">Sold by</span>
+              <span className="sold-by-brand">{item.sold_by_brand}</span>
+            </div>
+            <div className="item-name">{item.name}</div>
+            <div className="item-color">{item.color}</div>
+          </div>
+          <div className="desc-right">
+            <div className="org-price price">${item.price_in_cents / 1000}</div>
+            <div className="sale-price price">
+              ${item.sale_price_in_cents / 1000}
+            </div>
+          </div>
+        </div>
+        <div className="btns-and-collapse">
+          <div className="cart-small-btns">
+            <div className="yz-sm-btn">Buy now</div>
+            <div className="yz-sm-btn">Add to cart</div>
+          </div>
+          <div className="yz-icon">
+            <img src={downvote} alt="Collapse"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Product
